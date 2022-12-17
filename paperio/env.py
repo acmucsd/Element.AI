d6bb15a4fafc027a7c6775e665fcd515f25a729f
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2022-12-17 10:40:38 -0800

    Chore: organize functions

--- paperio/env.py
+++ paperio/env.py
@@ -123,6 +123,58 @@ class ACMAI2022(AECEnv):
 
 
     """
+    Space Functions
+    """
+    # TODO: Remove `Optional` type below
+    def observe(self, agent: str) -> Optional[ObsType]:
+        """Returns the observation an agent currently can make.
+
+        `last()` calls this function.
+        """
+        raise NotImplementedError
+
+    def observation_space(self, agent: AgentID) -> gymnasium.spaces.Space:
+        """Takes in agent and returns the observation space for that agent.
+
+        MUST return the same value for the same agent name
+
+        Default implementation is to return the observation_spaces dict
+        """
+        warnings.warn(
+            "Your environment should override the observation_space function. Attempting to use the observation_spaces dict attribute."
+        )
+        return self.observation_spaces[agent]
+
+    def action_space(self, agent: str) -> gymnasium.spaces.Space:
+        """Takes in agent and returns the action space for that agent.
+
+        MUST return the same value for the same agent name
+
+        Default implementation is to return the action_spaces dict
+        """
+        warnings.warn(
+            "Your environment should override the action_space function. Attempting to use the action_spaces dict attribute."
+        )
+        return self.action_spaces[agent]
+
+    def last(
+        self, observe: bool = True
+    ) -> Tuple[Optional[ObsType], float, bool, bool, Dict[str, Any]]:
+        """Returns observation, cumulative reward, terminated, truncated, info for the current agent (specified by self.agent_selection)."""
+        agent = self.agent_selection
+        assert agent
+        observation = self.observe(agent) if observe else None
+        return (
+            observation,
+            self._cumulative_rewards[agent],
+            self.terminations[agent],
+            self.truncations[agent],
+            self.infos[agent],
+        )
+
+
+
+    """
     Env Runtime Functions
     """
 
@@ -140,20 +192,6 @@ class ACMAI2022(AECEnv):
         options: Optional[dict] = None,
     ) -> None:
         """Resets the environment to a starting state."""
-        raise NotImplementedError
-
-    def seed(self, seed: Optional[int] = None) -> None:
-        """Reseeds the environment (making the resulting environment deterministic)."""
-        raise NotImplementedError(
-            "Calling seed externally is deprecated; call reset(seed=seed) instead"
-        )
-
-    # TODO: Remove `Optional` type below
-    def observe(self, agent: str) -> Optional[ObsType]:
-        """Returns the observation an agent currently can make.
-
-        `last()` calls this function.
-        """
         raise NotImplementedError
 
     def render(self):
@@ -185,29 +223,26 @@ class ACMAI2022(AECEnv):
         """
         pass
 
-    def observation_space(self, agent: AgentID) -> gymnasium.spaces.Space:
-        """Takes in agent and returns the observation space for that agent.
+    # NOTE: seed will be useful only when we implement seed-based bomb/boost placement
+    # def seed(self, seed: Optional[int] = None) -> None:
+    #     """Reseeds the environment (making the resulting environment deterministic)."""
+    #     raise NotImplementedError(
+    #         "Calling seed externally is deprecated; call reset(seed=seed) instead"
+    #     )
 
-        MUST return the same value for the same agent name
+    # NOTE: note useful rn
+    # def agent_iter(self, max_iter: int = 2**63) -> AECIterable:
+    #     """Yields the current agent (self.agent_selection).
 
-        Default implementation is to return the observation_spaces dict
-        """
-        warnings.warn(
-            "Your environment should override the observation_space function. Attempting to use the observation_spaces dict attribute."
-        )
-        return self.observation_spaces[agent]
+    #     Needs to be used in a loop where you step() each iteration.
+    #     """
+    #     return AECIterable(self, max_iter)
 
-    def action_space(self, agent: str) -> gymnasium.spaces.Space:
-        """Takes in agent and returns the action space for that agent.
 
-        MUST return the same value for the same agent name
 
-        Default implementation is to return the action_spaces dict
-        """
-        warnings.warn(
-            "Your environment should override the action_space function. Attempting to use the action_spaces dict attribute."
-        )
-        return self.action_spaces[agent]
+    """
+    Properties
+    """
 
     @property
     def num_agents(self) -> int:
@@ -217,20 +252,15 @@ class ACMAI2022(AECEnv):
     def max_num_agents(self) -> int:
         return len(self.possible_agents)
 
-    def _deads_step_first(self) -> AgentID:
-        """Makes .agent_selection point to first terminated agent.
+    @property
+    def unwrapped(self) -> AECEnv:
+        return self
 
-        Stores old value of agent_selection so that _was_dead_step can restore the variable after the dead agent steps.
-        """
-        _deads_order = [
-            agent
-            for agent in self.agents
-            if (self.terminations[agent] or self.truncations[agent])
-        ]
-        if _deads_order:
-            self._skip_agent_selection = self.agent_selection
-            self.agent_selection = _deads_order[0]
-        return self.agent_selection
+
+    
+    """
+    Rewards
+    """
 
     def _clear_rewards(self) -> None:
         """Clears all items in .rewards."""
@@ -245,85 +275,80 @@ class ACMAI2022(AECEnv):
         for agent, reward in self.rewards.items():
             self._cumulative_rewards[agent] += reward
 
-    def agent_iter(self, max_iter: int = 2**63) -> AECIterable:
-        """Yields the current agent (self.agent_selection).
 
-        Needs to be used in a loop where you step() each iteration.
-        """
-        return AECIterable(self, max_iter)
 
-    def last(
-        self, observe: bool = True
-    ) -> Tuple[Optional[ObsType], float, bool, bool, Dict[str, Any]]:
-        """Returns observation, cumulative reward, terminated, truncated, info for the current agent (specified by self.agent_selection)."""
-        agent = self.agent_selection
-        assert agent
-        observation = self.observe(agent) if observe else None
-        return (
-            observation,
-            self._cumulative_rewards[agent],
-            self.terminations[agent],
-            self.truncations[agent],
-            self.infos[agent],
-        )
+    """
+    Dead Agent Handlers
+    NOTE: Not used by our env since we respawn players
+    """
+    # def _deads_step_first(self) -> AgentID:
+    #     """Makes .agent_selection point to first terminated agent.
 
-    def _was_dead_step(self, action: None) -> None:
-        """Helper function that performs step() for dead agents.
+    #     Stores old value of agent_selection so that _was_dead_step can restore the variable after the dead agent steps.
+    #     """
+    #     _deads_order = [
+    #         agent
+    #         for agent in self.agents
+    #         if (self.terminations[agent] or self.truncations[agent])
+    #     ]
+    #     if _deads_order:
+    #         self._skip_agent_selection = self.agent_selection
+    #         self.agent_selection = _deads_order[0]
+    #     return self.agent_selection
 
-        Does the following:
+    # def _was_dead_step(self, action: None) -> None:
+    #     """Helper function that performs step() for dead agents.
 
-        1. Removes dead agent from .agents, .terminations, .truncations, .rewards, ._cumulative_rewards, and .infos
-        2. Loads next agent into .agent_selection: if another agent is dead, loads that one, otherwise load next live agent
-        3. Clear the rewards dict
+    #     Does the following:
 
-        Examples:
-            Highly recommended to use at the beginning of step as follows:
+    #     1. Removes dead agent from .agents, .terminations, .truncations, .rewards, ._cumulative_rewards, and .infos
+    #     2. Loads next agent into .agent_selection: if another agent is dead, loads that one, otherwise load next live agent
+    #     3. Clear the rewards dict
 
-        def step(self, action):
-            if (self.terminations[self.agent_selection] or self.truncations[self.agent_selection]):
-                self._was_dead_step()
-                return
-            # main contents of step
-        """
-        if action is not None:
-            raise ValueError("when an agent is dead, the only valid action is None")
+    #     Examples:
+    #         Highly recommended to use at the beginning of step as follows:
 
-        # removes dead agent
-        agent = self.agent_selection
-        assert (
-            self.terminations[agent] or self.truncations[agent]
-        ), "an agent that was not dead as attempted to be removed"
-        del self.terminations[agent]
-        del self.truncations[agent]
-        del self.rewards[agent]
-        del self._cumulative_rewards[agent]
-        del self.infos[agent]
-        self.agents.remove(agent)
+    #     def step(self, action):
+    #         if (self.terminations[self.agent_selection] or self.truncations[self.agent_selection]):
+    #             self._was_dead_step()
+    #             return
+    #         # main contents of step
+    #     """
+    #     if action is not None:
+    #         raise ValueError("when an agent is dead, the only valid action is None")
 
-        # finds next dead agent or loads next live agent (Stored in _skip_agent_selection)
-        _deads_order = [
-            agent
-            for agent in self.agents
-            if (self.terminations[agent] or self.truncations[agent])
-        ]
-        if _deads_order:
-            if getattr(self, "_skip_agent_selection", None) is None:
-                self._skip_agent_selection = self.agent_selection
-            self.agent_selection = _deads_order[0]
-        else:
-            if getattr(self, "_skip_agent_selection", None) is not None:
-                assert self._skip_agent_selection is not None
-                self.agent_selection = self._skip_agent_selection
-            self._skip_agent_selection = None
-        self._clear_rewards()
+    #     # removes dead agent
+    #     agent = self.agent_selection
+    #     assert (
+    #         self.terminations[agent] or self.truncations[agent]
+    #     ), "an agent that was not dead as attempted to be removed"
+    #     del self.terminations[agent]
+    #     del self.truncations[agent]
+    #     del self.rewards[agent]
+    #     del self._cumulative_rewards[agent]
+    #     del self.infos[agent]
+    #     self.agents.remove(agent)
 
-    def __str__(self) -> str:
-        """Returns a name which looks like: `space_invaders_v1`."""
-        if hasattr(self, "metadata"):
-            return self.metadata.get("name", self.__class__.__name__)
-        else:
-            return self.__class__.__name__
+    #     # finds next dead agent or loads next live agent (Stored in _skip_agent_selection)
+    #     _deads_order = [
+    #         agent
+    #         for agent in self.agents
+    #         if (self.terminations[agent] or self.truncations[agent])
+    #     ]
+    #     if _deads_order:
+    #         if getattr(self, "_skip_agent_selection", None) is None:
+    #             self._skip_agent_selection = self.agent_selection
+    #         self.agent_selection = _deads_order[0]
+    #     else:
+    #         if getattr(self, "_skip_agent_selection", None) is not None:
+    #             assert self._skip_agent_selection is not None
+    #             self.agent_selection = self._skip_agent_selection
+    #         self._skip_agent_selection = None
+    #     self._clear_rewards()
 
-    @property
-    def unwrapped(self) -> AECEnv:
-        return self
+    # def __str__(self) -> str:
+    #     """Returns a name which looks like: `space_invaders_v1`."""
+    #     if hasattr(self, "metadata"):
+    #         return self.metadata.get("name", self.__class__.__name__)
+    #     else:
+    #         return self.__class__.__name__
