b037cc3304028a36a52c9b72edca3266699243f6
Jonah <volcanobud@gmail.com>
2023-01-22 22:18:18 -0800

    added lava texture, removed old code

--- paperio/visualizer.py
+++ paperio/visualizer.py
@@ -15,19 +15,25 @@ class Visualizer:
         self.WINDOW_HEIGHT = self.block_height * size;
         self.WINDOW_WIDTH = self.block_width * size;
         self.map_size = size
-        self.screen_size = (size*10, size*10)
-        self.tile_width = min(self.screen_size[0] // size, self.screen_size[1] // size)
-        self.WINDOW_SIZE = (self.tile_width * size, self.tile_width * size)
-        # self.surf = pygame.Surface(self.WINDOW_SIZE)
-        # self.surf.fill([239, 120, 79])
-        # pygame.font.init()
-        # self.screen = None
-        self.rgb_array = np.zeros((self.WINDOW_SIZE[0],self.WINDOW_SIZE[1], 3), dtype=np.uint8)
+        # self.screen_size = (size*10, size*10)
+        # self.tile_width = min(self.screen_size[0] // size, self.screen_size[1] // size)
+        # self.WINDOW_SIZE = (self.tile_width * size, self.tile_width * size)
+        # # self.surf = pygame.Surface(self.WINDOW_SIZE)
+        # # self.surf.fill([239, 120, 79])
+        # # pygame.font.init()
+        # # self.screen = None
+        self.rgb_array = np.zeros((self.WINDOW_WIDTH,self.WINDOW_HEIGHT, 3), dtype=np.uint8)
 
         self.bomb_img = pygame.transform.scale(pygame.image.load('bomb1.jpg'), (self.block_width, self.block_height))
-        #self.bomb_array = pygame.surfarray.array3d(bomb_img)
-        self.wave_img = pygame.transform.scale(pygame.image.load('wave.png'), (self.block_width, self.block_width))
         self.boost_img = pygame.transform.scale(pygame.image.load('BOOST2.png'), (self.block_width, self.block_width))
+
+        self.water_img = pygame.transform.scale(pygame.image.load('water3.png'), (self.block_width, self.block_width))
+        self.water_img_move = pygame.transform.scale(pygame.image.load('waterMoving.png'), (self.block_width, self.block_width))
+
+        self.fire_img = pygame.transform.scale(pygame.image.load('lavaTexture.png'), (self.block_width, self.block_width))
+        self.fire_img_move = pygame.transform.scale(pygame.image.load('lavaTextureMove.png'),
+                                                     (self.block_width, self.block_width))
+
         #self.boost_array = pygame.surfarray.array3d(boost_img)
         self.eps = 0
         #print(self.rgbarray.ndim)
@@ -43,49 +49,26 @@ class Visualizer:
         #self.draw_grid()
         pygame.display.update()
 
-    def draw_grid(self):
-        for x in range(0, self.WINDOW_WIDTH, self.block_width):
-            for y in range(0, self.WINDOW_HEIGHT, self.block_height):
-                rect = pygame.Rect(x, y, self.block_width, self.block_height)
-                pygame.draw.rect(self.screen, color.BLACK, rect, 1)
+    # def draw_grid(self):
+    #     for x in range(0, self.WINDOW_WIDTH, self.block_width):
+    #         for y in range(0, self.WINDOW_HEIGHT, self.block_height):
+    #             rect = pygame.Rect(x, y, self.block_width, self.block_height)
+    #             pygame.draw.rect(self.screen, color.BLACK, rect, 1)
 
     def render(self):
+        pygame.event.pump()
         pygame.display.update();
-        print("hi")
-        #self.draw_grid();
-        #fStack = np.hstack((self.rgbarray, self.rgbarray, self.rgbarray, self.rgbarray))
-        #self.surf = pygame.surfarray.make_surface(np.vstack((fStack, fStack, fStack, fStack)))
-        # self.surf = pygame.surfarray.make_surface(self.rgb_array)
-        # pygame.event.pump()
-        # #self.surf = pygame.surfarray.make_surface(smallbomb)
-        # #self.surf = pygame.surfarray.array3d(bombimg)
-        # #imgsurface = pygame.image.load('bomb.jpg')
-        # #rgbarray = pygame.surfarray.array3d(imgsurface)
-        # #pygame.examples.surfdemo_show(rgbarray, 'rgbarray')
-        # #pygame.display.update()
-        # #resized = pygame.transform.scale(self.surf, self.screen.get_rect().size)
-        # self.screen.blit(self.surf, (0, 0))
-        # print('updateScene ' + str(self.eps))
-        # self.eps += 1
-        # pygame.display.update()
-
-    def update_block(self, img, row, col):
-        for r in range(self.tile_width):
-            for c in range(self.tile_width):
-                self.rgb_array[row+r][col+c] = img[r][c]
-
-    def update_color(self, color, row, col):
-        for r in range(self.tile_width):
-            for c in range(self.tile_width):
-                self.rgb_array[row+ r][col + c] = color
 
     def update_scene(self, grid, player_num_grid):
-        self.player_colors = [
-                (np.array(color.YELLOW_ORANGE, dtype=np.uint8), np.array(color.SAPPHIRE_BLUE, dtype=np.uint8)),
-                (np.array(color.HOT_PINK, dtype=np.uint8), np.array(color.SAP_GREEN, dtype=np.uint8)),
-                (np.array(color.RED_ORANGE, dtype=np.uint8), np.array(color.TEAL, dtype=np.uint8)),
-                (np.array(color.GOLD, dtype=np.uint8), np.array(color.PURPLE_HEART, dtype=np.uint8)),
-            ]
+        # self.player_colors = [
+        #         (np.array(color.YELLOW_ORANGE, dtype=np.uint8), np.array(color.SAPPHIRE_BLUE, dtype=np.uint8)),
+        #         (np.array(color.HOT_PINK, dtype=np.uint8), np.array(color.SAP_GREEN, dtype=np.uint8)),
+        #         (np.array(color.RED_ORANGE, dtype=np.uint8), np.array(color.TEAL, dtype=np.uint8)),
+        #         (np.array(color.GOLD, dtype=np.uint8), np.array(color.PURPLE_HEART, dtype=np.uint8)),
+        #     ]
+        self.player_imgs = [(self.water_img_move, self.water_img),
+                            (self.fire_img_move, self.fire_img)
+                            ]
         for r in range(self.map_size):
             for c in range(self.map_size):
                 x = r * self.block_width
@@ -108,52 +91,11 @@ class Visualizer:
                     #self.rgb_array[r][c] = self.player_colors[player_num_grid[r][c]][0]
                     #pygame.draw.rect(self.screen, self.player_colors[player_num_grid[r][c]][0], rect, 0)
                     # pygame.draw.rect(self.screen, color.BLACK, rect, 1)
-                    self.screen.blit(self.wave_img, (x,y))
+                    self.screen.blit(self.player_imgs[player_num_grid[r][c]][0], (x,y))
                 elif grid[r][c] == OCCUPIED:
                     #self.rgb_array[r][c] = self.player_colors[player_num_grid[r][c]][1]
-                    pygame.draw.rect(self.screen, self.player_colors[player_num_grid[r][c]][1], rect, 0)
+                    self.screen.blit(self.player_imgs[player_num_grid[r][c]][1], (x,y))
                 else:
                     raise Exception("Unknown grid value")
-        #self.rgb_array = np.zeros((self.map_size, self.map_size, 3), dtype=np.uint8)
-        #
-        # self.player_colors = [
-        #         (np.array(color.YELLOW_ORANGE, dtype=np.uint8), np.array(color.SAPPHIRE_BLUE, dtype=np.uint8)),
-        #         (np.array(color.HOT_PINK, dtype=np.uint8), np.array(color.SAP_GREEN, dtype=np.uint8)),
-        #         (np.array(color.RED_ORANGE, dtype=np.uint8), np.array(color.TEAL, dtype=np.uint8)),
-        #         (np.array(color.GOLD, dtype=np.uint8), np.array(color.PURPLE_HEART, dtype=np.uint8)),
-        #     ]
-        #
-        # WHITE_SMOKE = np.array(color.WHITE_SMOKE)
-        # BLACK = np.array(color.BLACK)
-        # PURPLE = np.array(color.PURPLE)
-        # #print(self.map_size)
-        # for r in range(self.map_size):
-        #     #row = np.empty((0,0,0))
-        #     for c in range(self.map_size):
-        #         if grid[r][c] == UNOCCUPIED:
-        #             #self.rgb_array[r][c] = WHITE_SMOKE
-        #             self.update_color(WHITE_SMOKE, r*self.tile_width, c*self.tile_width)
-        #         elif grid[r][c] == BOMB:
-        #             #self.rgb_array[r][c] = BLACK
-        #             self.update_block(self.bomb_array, r*self.tile_width, c*self.tile_width)
-        #         elif grid[r][c] == BOOST:
-        #             #self.rgb_array[r][c] = PURPLE
-        #             self.update_block(self.boost_array, r*self.tile_width, c*self.tile_width)
-        #         elif grid[r][c] == PASSED:
-        #             #self.rgb_array[r][c] = self.player_colors[player_num_grid[r][c]][0]
-        #             self.update_color(self.player_colors[player_num_grid[r][c]][0], r*self.tile_width, c*self.tile_width)
-        #         elif grid[r][c] == OCCUPIED:
-        #             #self.rgb_array[r][c] = self.player_colors[player_num_grid[r][c]][1]
-        #             self.update_color(self.player_colors[player_num_grid[r][c]][1], r*self.tile_width, c*self.tile_width)
-        #         else:
-        #             raise Exception("Unknown grid value")
-        #
-        #         #r += self.tile_width
-        #     #c += self.tile_width
-        #     #np.vstack((self.rgb_array,row))
-        # #print("running")
-        #
-        # return self.rgb_array
-        # fStack = np.hstack((self.rgbarray,self.rgbarray,self.rgbarray,self.rgbarray))
-        # return np.vstack((fStack, fStack, fStack, fStack))
+        self.rgb_array = pygame.surfarray.array3d(self.screen)
 
