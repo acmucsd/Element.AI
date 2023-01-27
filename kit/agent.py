0921e27d04bb3eff922fe2d6f9dc2fd881a1b778
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2023-01-27 11:56:21 -0800

    Fix: Bombs and Boosts ignored by occupancy function

--- kit/agent.py
+++ kit/agent.py
@@ -1,5 +1,12 @@
 import numpy as np
 import sys
+
+TEMP = -1
+UNOCCUPIED = 0
+PASSED = 1
+OCCUPIED = 2
+BOMB = 3
+BOOST = 4
 class Agent():
     def __init__(self, player: str) -> None:
         self.player = player
@@ -9,7 +16,15 @@ class Agent():
 
         obs, rewards, dones, infos = obs
 
-        direction = iter % 3 - 1
+        board = np.array(obs['board']['board_state'])
+        player_owned = np.array(obs['board']['players_state'])
+        player_num = obs[self.player]['player_num']
+
+        occupied_territory = np.where(np.logical_and(board != PASSED, player_owned == player_num))
+        print(len(occupied_territory[0]), file=sys.stderr)
+
+        direction = 0
+        if iter % 10 == 0 and iter != 0: direction = 1
 
         action = {
             'turn': direction
