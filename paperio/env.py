f4a20e6f51b07e0ee9033312f4058695827dc466
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2023-01-28 18:58:05 -0800

    Feat: Redo update_occupancy func

--- paperio/env.py
+++ paperio/env.py
@@ -362,42 +362,45 @@ class PaperIO(ParallelEnv):
         player.reset_player()
 
     def update_occupancy(self, player: Player):
-
         map_size = self.env_cfg.map_size
-        queue = collections.deque([])
-        for r in range(map_size):
-            for c in range(map_size):
-                if (r in [0, map_size-1] or c in [0, map_size-1]) and (self.grid[r][c] in [UNOCCUPIED, BOMB, BOOST]):
-                    queue.append((r, c))
+        indices = np.moveaxis(np.mgrid[:map_size,:map_size], 0, -1)
+        edge = np.concatenate((indices[0,:], indices[map_size-1,:], indices[1:map_size-1,0], indices[1:map_size-1,map_size-1]))
+
+        OCCUPIED_TARGS = [UNOCCUPIED, BOMB, BOOST]
+
+        queue = collections.deque(edge.tolist())
+        explored = np.full((map_size, map_size), False)
         while queue:
             r, c = queue.popleft()
-            if 0<=r<map_size and 0<=c<map_size and (self.grid[r][c] in [UNOCCUPIED, BOMB, BOOST]):
-                if (self.grid[r][c] == UNOCCUPIED): self.grid[r][c] = TEMP
-                if (self.grid[r][c] == BOMB): self.grid[r][c] = -1 * BOMB
-                if (self.grid[r][c] == BOOST): self.grid[r][c] = -1 * BOOST
-                queue.extend([(r-1, c),(r+1, c),(r, c-1),(r, c+1)])
 
-        for r in range(map_size):
-            for c in range(map_size):
-                cell = self.grid[r][c]
-                occupied_by = self.player_grid[r][c]
-                if (occupied_by == player and cell == PASSED) or (cell in [UNOCCUPIED, BOMB, BOOST]):
-                    self.grid[r][c] = OCCUPIED
-                    self.player_grid[r][c] = player
-                    self.player_num_grid[r][c] = player.num
-                    player.push_zone((c,r))
-                elif cell == TEMP:
-                    self.grid[r][c] = UNOCCUPIED
-                    self.player_grid[r][c] = None
-                    self.player_num_grid[r][c] = -1
-                elif cell == -1 * BOMB:
-                    self.grid[r][c] = BOMB
-                    self.player_grid[r][c] = None
-                    self.player_num_grid[r][c] = -1
-                elif cell == -1 * BOOST:
-                    self.grid[r][c] = BOOST
-                    self.player_grid[r][c] = None
-                    self.player_num_grid[r][c] = -1
+            if (0 <= r < map_size and 0 <= c < map_size) and (not explored[r,c]):
+                explored[r,c] = True
+            
+                cell = self.grid[r,c]
+                occ_num = self.player_num_grid[r,c]
+
+                if (cell in OCCUPIED_TARGS) or (occ_num != player.num):
+
+                    self.grid[r,c] = TEMP if cell == UNOCCUPIED else TEMP * BOMB if cell == BOMB else TEMP * BOOST if cell == BOOST else self.grid[r,c]
+
+                    queue.extend([(r-1, c),(r+1, c),(r, c-1),(r, c+1)])
+
+        grid_bomb_or_boost = np.logical_or(self.grid == BOMB, self.grid == BOOST)
+        grid_occupied_targ = np.logical_or(self.grid == UNOCCUPIED, grid_bomb_or_boost)
+        grid_player_passed = np.logical_and(self.grid == PASSED, self.player_num_grid == player.num)
+        enclosed = np.logical_or(grid_player_passed, grid_occupied_targ)
+
+        free_tile = self.grid == TEMP
+        free_bomb = self.grid == TEMP * BOMB
+        free_boost = self.grid == TEMP * BOOST
+
+        self.grid[enclosed] = OCCUPIED
+        self.player_grid[enclosed] = player
+        self.player_num_grid[enclosed] = player.num
+
+        self.grid[free_tile] = UNOCCUPIED
+        self.grid[free_bomb] = BOMB
+        self.grid[free_boost] = BOOST
 
     def reset(
         self,
