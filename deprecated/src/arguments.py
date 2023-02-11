bd818639115c37802c41b0b0797f4f64829a1c12
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2023-02-11 12:52:33 -0800

    Chore: more file management

--- deprecated/src/arguments.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--env', help='Environment to run. Either angular or grid', default='angular')
-
-def get_args():
-    return parser.parse_args()
